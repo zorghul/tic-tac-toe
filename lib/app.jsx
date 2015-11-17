a344e40bcae81f898d3f6be60d7d6b67fc97b113
BEZIAT Mayeul <mayeul.beziat@devinci.fr>
2015-11-17 15:45:43 +0100

    Etape 5
    TP4

--- lib/app.jsx
+++ lib/app.jsx
@@ -2,43 +2,34 @@
 
 var React = require('react');
 
+var ALTERNANCE_MS = 300;
+
 var boxStyle = {
 	'height':'100px',
 	'width':'100px'
 };
 
-/*
-var Counter = React.createClass({
-  getInitialState: function() {
-    // naming it initialX clearly indicates that the only purpose
-    // of the passed down prop is to initialize something internally
-    return {count: this.props.initialCount};
-  },
-
-  handleClick: function() {
-    this.setState({count: this.state.count + 1});
-  },
-
-  render: function() {
-    return <div onClick={this.handleClick}>{this.state.count}</div>;
-  }
-});
-
-ReactDOM.render(<Counter initialCount={7}/>, mountNode);
-*/
-
-
-
 var Box = React.createClass({
-	getInitialState: function() {
-	return (value : this.props.initialValue)
+	'getInitialState': function onGetInitialState() {
+	return {'value' : this.props.initialValue};
 	},
+	/*
+	'componentWillMount': function onComponentWillMount() {
+	var self = this;
+	
+	this.timer = setInterval(function onTimer (){
+	self.setState({
+	
+	})}
+	)}
+	*/
   'render': function onRender () {
     return (
-      <button style={boxStyle}>{this.state.value}</button>
+      <button style={boxStyle} >{this.state.value}</button>
     );
   }
 });
 
-React.render(<Box initialValue='Y'/>, document.body);
+
+React.render(<Box initialValue='X'/>, document.body);
 
